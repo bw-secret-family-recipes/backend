7f0782a4980f866e61d3e05d867088cc53bb9b46
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-10-23 08:05:29 -0400

    Additional dependencies, updated file structure

--- api/server.js
+++ api/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
+const cors = require('cors');
 
-const RecipesRouter = require('../data/recipes/recipes-router.js');
+const AuthRouter = require('./users/auth-router');
+const RecipesRouter = require('./recipes/recipes-router');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 server.use(helmet());
 server.use(logger('dev'));
 
 // Router
+server.use('/api/auth', AuthRouter);
 server.use('/api/recipes', RecipesRouter);
 
 // Server Check
