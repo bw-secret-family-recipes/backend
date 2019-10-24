dad4e503fb5b61ef4b9987cd3356264a3cf02eda
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-10-24 13:26:20 -0400

    Updating package.json

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,10 +2,13 @@ const knex = require('knex');
 
 const config = require('../knexfile.js');
 
+const dbEnv = process.env.DB_ENV || 'development';
+module.exports = knex(config[dbEnv]);
+
 // const dbEnv = process.env.DB_ENV || 'development';
 
 // module.exports = knex(config.development);
-module.exports = knex(config);
+// module.exports = knex(config);
 
 
 // const environment = process.env.ENVIRONMENT || 'development'
--- knexfile.js
+++ knexfile.js
@@ -19,22 +19,26 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done)
       }
     },
-    production: {
-      debug: true,
-      client: 'pg',
-      connection: 'postgres://robhbglcczgytm:5771f2a36931ec604ffa8291f4a829836787354be5a2bf76698108a88b86a54e@ec2-54-243-239-199.compute-1.amazonaws.com:5432/d7nb3hlr0jgsf9?ssl=true',
-      migrations: {
-        directory: './data/migrations'
-      },
-      ssl: true,
-      seeds: {
-        directory: './data/seeds'
-      },
-      pool: {
-        min: 2,
-        max: 10
-      }
+  },
+
+  production: {
+    debug: true,
+    client: 'pg',
+    connection: 'postgres://robhbglcczgytm:5771f2a36931ec604ffa8291f4a829836787354be5a2bf76698108a88b86a54e@ec2-54-243-239-199.compute-1.amazonaws.com:5432/d7nb3hlr0jgsf9?ssl=true',
+    migrations: {
+      directory: './data/migrations'
+    },
+    ssl: true,
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      min: 2,
+      max: 10
     }
   }
-}[process.env.NODE_ENV || "development"]
+}
+
+
+//[process.env.NODE_ENV || "development"]
 
