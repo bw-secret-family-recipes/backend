ed181ea86550f97837398417df39fb14ca84f0ab
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-10-24 08:29:28 -0400

    dbConfig update

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,11 +1,11 @@
 const knex = require('knex');
 
-// const config = require('../knexfile.js');
+const config = require('../knexfile.js');
 
-// const dbEnv = process.env.DB_ENV || 'development';
+const dbEnv = process.env.DB_ENV || 'development';
 
-// module.exports = knex(config.development[dbEnv]); 
+module.exports = knex(config.development[dbEnv]);
 
-const environment = process.env.ENVIRONMENT || 'development'
-const config = require('../knexfile.js')[environment];
-module.exports = ('knex')(config);
+// const environment = process.env.ENVIRONMENT || 'development'
+// const config = require('../knexfile.js')[environment];
+// module.exports = ('knex')(config);
